6fa1004af03076259d56e71e2a42d11fb8b90e47
Yann Coleu <y@nn-col.eu>
2019-12-03 10:00:50 +0100

    Add PORT variable usefull for Cloud Run context

--- start.sh
+++ start.sh
@@ -12,16 +12,24 @@ else
 	if [[ ${REDIRECT_TARGET:length-1:1} != "/" ]]; then
 		REDIRECT_TARGET="$REDIRECT_TARGET/"
 	fi
+fi
 
-	echo "Redirecting HTTP requests to ${REDIRECT_TARGET}..."
+# Default to 80
+export LISTEN="80"
+# Listen to PORT variable given on Cloud Run Context
+if [ ! -z "$PORT" ]; then
+	LISTEN="$PORT"
 fi
 
 cat <<EOF > /etc/nginx/conf.d/default.conf
 server {
-	listen 80;
+	listen ${LISTEN};
 
-	rewrite ^/(.*)\$ $REDIRECT_TARGET\$1 permanent;
+	rewrite ^/(.*)\$ ${REDIRECT_TARGET}\$1 permanent;
 }
 EOF
+
+
+echo "Listening to $LISTEN, Redirecting HTTP requests to ${REDIRECT_TARGET}..."
 
 exec nginx -g "daemon off;"
